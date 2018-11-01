d0ff252f9e0e0e476d974ac8e82e0aed06339c6b
Dylan Sherry <sharshofski@gmail.com>
2018-11-01 13:57:44 -0400

    Clean up prints

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const socket_server = io();
 function sendData(data) {
     artnet.set(1, 0, data,
                function (err, res) {
-                   console.log('sending');
-                   console.log(err);
-                   if (err) { console.log(err); }
-                   if (res) { console.log(res); }
+                   console.log('sending artnet message');
+                   if (err) { console.log('Error is ' + err); }
+                   if (res) { console.log('Response is ' + res); }
                    artnet.close();
                });
 }
@@ -21,7 +20,8 @@ function sendData(data) {
 socket_server.on('connection', function(socket){
     console.log('Socket received conenction');
     socket.on('data', function(msg){
-        console.log('message: ' + msg);
+        console.log('Message received by socket is: ' + msg);
+        sendData(msg);
     });
 });
 socket_server.listen(1337);
@@ -29,4 +29,5 @@ socket_server.listen(1337);
 // for testing
 const io_client = require('socket.io-client');
 const socket_client = io_client('http://localhost:1337');
-socket_client.emit('data', 'derp')
+socket_client.emit('data', 'test');
+socket_client.emit('data', [255, 255]);
