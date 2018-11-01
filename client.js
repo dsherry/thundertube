a20d031819c337d5738d410035a5e707073c570e
strickinato <aaronstrick@gmail.com>
2018-11-01 16:58:24 -0400

    add client

---
+++ client.js
@@ -0,0 +1,40 @@
+var socket = io("http://localhost:1337");
+
+var editor = CodeMirror(document.getElementById("mount"), {
+  value: "// Define a function called draw that takes a timestampe (1)\nfunction draw(tick){\n  return Array(512).fill(244);\n}",
+  mode:  "javascript",
+});
+
+
+let shouldRepeat = true;
+
+editor.on("changes", function(){
+  let text = editor.getValue();
+  let messageDiv = document.getElementById("message");
+  shouldRepeat = false;
+
+  try {
+    eval(text);
+    messageDiv.innerHTML = "\n";
+
+    try {
+      let send = function() {
+        let tick = Date.now();
+        let array = eval("draw(tick)");
+
+        console.log("sending");
+        socket.emit("data", array);
+        messageDiv.innerHTML = "[" + array.toString() + "]";
+        requestAnimationFrame(send);
+      }
+
+      window.requestAnimationFrame(send);
+
+    } catch(err) {
+      message.innerHTML = err;
+    }
+
+  } catch(err) {
+    message.innerHTML = err;
+  }
+});
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var options = {
-    host: '10.0.18.242'
+    host: '10.0.18.252'
 }
 const artnet = require('artnet')(options);
 const io = require('socket.io');
 const socket_server = io();
 
-function sendData(data) {
-    artnet.set(1, 0, data,
+function sendData(data, sequence) {
+  console.log(sequence);
+  artnet.set(1, 0, sequence, data,
                function (err, res) {
                    console.log('sending artnet message');
                    if (err) { console.log('Error is ' + err); }
                    if (res) { console.log('Response is ' + res); }
-                   artnet.close();
                });
 }
 
@@ -24,9 +24,10 @@ function validateData(data) {
 socket_server.on('connection', function(socket){
     console.log('Socket received conenction');
     socket.on('data', function(msg){
-        console.log('Message received by socket is: ' + msg);
+      console.log("about to validate");
         if (validateData(msg)) {
-            sendData(msg);
+            console.log('Message received by socket is: ');
+            sendData(msg, Math.floor(Math.random() * 100));
         };
     });
 });
@@ -37,7 +38,7 @@ const io_client = require('socket.io-client');
 const socket_client = io_client('http://localhost:1337');
 socket_client.emit('data', 'test');
 socket_client.emit('data', [1,2,3]);
-socket_client.emit('data', testArray());
+// socket_client.emit('data', testArray());
 
 function testArray() {
   return Array.apply(null, new Array(512)).map(function () { return 255; }, 0);
