11306691ee8b8cbdc2a5028d298d97a562611e6f
Dylan Sherry <sharshofski@gmail.com>
2018-11-01 13:50:30 -0400

    Got simple client/server working

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var options = {
     host: '10.0.18.242'
 }
 const artnet = require('artnet')(options);
-
-
+const io = require('socket.io');
+const socket_server = io();
 
 function sendData(data) {
     artnet.set(1, 0, data,
@@ -16,6 +16,17 @@ function sendData(data) {
                });
 }
 
-function 
+// Array.apply(null, new Array(512)).map(function () { return 255; }, 0)
 
-Array.apply(null, new Array(512)).map(function () { return 255; }, 0)
+socket_server.on('connection', function(socket){
+    console.log('Socket received conenction');
+    socket.on('data', function(msg){
+        console.log('message: ' + msg);
+    });
+});
+socket_server.listen(1337);
+
+// for testing
+const io_client = require('socket.io-client');
+const socket_client = io_client('http://localhost:1337');
+socket_client.emit('data', 'derp')
