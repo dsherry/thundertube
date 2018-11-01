eb636dd7834d36b608b10d72a0e737a85e7632ae
Dylan Sherry <sharshofski@gmail.com>
2018-11-01 17:45:09 -0400

    Refactor client loop. Still not sending correct data

--- client.js
+++ client.js
@@ -1,40 +1,49 @@
 var socket = io("http://localhost:1337");
 
+let defaultText = "// Define a function called draw that takes a timestampe (1)\nfunction draw(tick){\n  return Array(512).fill(127);\n}";
+
 var editor = CodeMirror(document.getElementById("mount"), {
-  value: "// Define a function called draw that takes a timestampe (1)\nfunction draw(tick){\n  return Array(512).fill(127);\n}",
+  value: defaultText,
   mode:  "javascript",
 });
 
 
-let shouldRepeat = true;
+let currentCode = defaultText;
+let codeChanged = false;
 
 editor.on("changes", function(){
-  let text = editor.getValue();
+  let userCode = editor.getValue();
   let messageDiv = document.getElementById("message");
-  shouldRepeat = false;
 
   try {
-    eval(text);
-    messageDiv.innerHTML = "\n";
+    eval(userCode);
+      currentCode = userCode;
+      codeChanged = true;
+      messageDiv.innerHTML = "\n";
 
-    try {
-      let send = function() {
-        let tick = Date.now();
-        let array = eval("draw(tick)");
-
-        console.log("sending");
-        socket.emit("data", array);
-        messageDiv.innerHTML = "[" + array.toString() + "]";
-        requestAnimationFrame(send);
-      }
-
-      window.requestAnimationFrame(send);
-
-    } catch(err) {
-      message.innerHTML = err;
-    }
 
   } catch(err) {
     message.innerHTML = err;
   }
 });
+
+function start() {
+    let send = function() {
+        try {
+            if (codeChanged) {
+                codeChanged = false;
+                eval(currentCode);
+            }
+            let tick = Date.now();
+            let array = eval("draw(tick)");
+            console.log("sending");
+            socket.emit("data", array);
+            messageDiv.innerHTML = "[" + array.toString() + "]";
+        } catch(err) {
+            message.innerHTML = err;
+        }
+        requestAnimationFrame(send);
+    }
+    window.requestAnimationFrame(send);
+};
+start();
