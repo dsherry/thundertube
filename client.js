eaf7d812d52050a6dc0b0d958a2c08291ba24a07
Dylan Sherry <sharshofski@gmail.com>
2018-11-01 17:16:53 -0400

    Changed default color

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 var socket = io("http://localhost:1337");
 
 var editor = CodeMirror(document.getElementById("mount"), {
-  value: "// Define a function called draw that takes a timestampe (1)\nfunction draw(tick){\n  return Array(512).fill(244);\n}",
+  value: "// Define a function called draw that takes a timestampe (1)\nfunction draw(tick){\n  return Array(512).fill(127);\n}",
   mode:  "javascript",
 });
 
