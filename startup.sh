92041373645627402fd64313e2ef6a66753467f2
strickinato <aaronstrick@gmail.com>
2018-11-30 16:16:38 -0500

    add startup script

---
+++ startup.sh
@@ -0,0 +1,8 @@
+cd /home/pi/thundertube
+node server/index.js
+
+# Ensures the server has enough time to start up
+sleep 6
+
+# kiosk mode gives a great fullscreen experience
+chromium-browser --kiosk http://localhost:5000
